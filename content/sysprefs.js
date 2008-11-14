a0b14ee5b5c6010d8197562cee8d82864a87f827
user <user@ubuntu804desktop.localdomain>
2008-11-14 18:16:58 +0800

    Added preferences for precision and rounding.

--- content/sysprefs.js
+++ content/sysprefs.js
@@ -7,8 +7,15 @@
      * Controller Startup
      */
     function startup() {
+        GeckoJS.Configure.loadPreferences('vivipos.fec.settings');
+        var rt = GeckoJS.Configure.read('vivipos.fec.settings.RoundingTaxes') || 'to-nearest-precision';
+        var rp = GeckoJS.Configure.read('vivipos.fec.settings.RoundingPrices') || 'to-nearest-precision';
+
+        var rpNode = document.getElementById('roundingprices');
+        if (rpNode) rpNode.value = rp;
         
-        
+        var rtNode = document.getElementById('roundingtaxes');
+        if (rtNode) rtNode.value = rt;
     };
 
     window.addEventListener('load', startup, false);
