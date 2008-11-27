c21e7ab51c9b9ec5740cc3745c3e8e6dca6202d2
AChang Hsiao <achangh@gmail.com>
2008-11-27 14:16:29 +0800

    vivipos app, achang

--- content/pricelevelschedule.js
+++ content/pricelevelschedule.js
@@ -2,14 +2,17 @@
 
     // include controllers  and register itself
 
-    // GeckoJS.include('chrome://viviecr/content/controllers/pricelevelschedule_controller.js');
+    GeckoJS.include('chrome://viviecr/content/controllers/pricelevelschedule_controller.js');
 
     /**
      * Controller Startup
      */
     function startup() {
+//        $("#simpleListBoxSchedule")[0].addEventListener('select', function(evt) {
+//            $do('select', evt, 'PriceLevelSchedule');
+//        }, false);
 
-        // $do('load', null, 'Pricelevelschedule');
+        $do('load', null, 'PriceLevelSchedule');
 
     };
 
