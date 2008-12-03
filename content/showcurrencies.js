e366ce4e632ec9b4bf9b831fa20db15f2fa06832
AChang Hsiao <achangh@gmail.com>
2008-12-03 12:10:57 +0800

    aa

---
+++ content/showcurrencies.js
@@ -0,0 +1,18 @@
+(function(){
+
+    // include controllers  and register itself
+
+    /**
+     * Controller Startup
+     */
+    function startup() {
+
+        // $do('load', null, 'Promotions');
+
+    };
+
+    window.addEventListener('load', startup, false);
+
+})();
+
+
