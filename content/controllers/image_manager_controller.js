8c8d14dd05529941c36456c99d0f42e3ec56dfd2
user <user@ubuntu804desktop.localdomain>
2008-12-06 01:26:07 +0800

    Revised Image Manager.
    Fixed bug in funcpanelecrprefs.css that causes vivitree scrollbars to
    misbehave.

--- content/controllers/image_manager_controller.js
+++ content/controllers/image_manager_controller.js
@@ -46,6 +46,7 @@ var ImageFilesView = window.ImageFilesView = GeckoJS.NSITreeViewArray.extend({
         _selectedIndex: -1,
         _disklimit: 1 * 1024 * 1024, // 50 MB
         _importDir: null,
+        _exportDir: null,
 
         loadImage: function(dir) {
 
@@ -59,7 +60,9 @@ var ImageFilesView = window.ImageFilesView = GeckoJS.NSITreeViewArray.extend({
             if (limitSetting > this._disklimit) this._disklimit = limitSetting;
 
             this._importDir = GeckoJS.Configure.read('vivipos.fec.settings.image.importdir');
+            this._exportDir = GeckoJS.Configure.read('vivipos.fec.settings.image.exportdir');
             if (!this._importDir) this._importDir = '/media/disk/image_import/';
+            if (!this._exportDir) this._exportDir = '/media/disk/image_export/';
 
             this.query('#imagePanel')[0].datasource = this.imagefilesView;
 
@@ -107,34 +110,107 @@ var ImageFilesView = window.ImageFilesView = GeckoJS.NSITreeViewArray.extend({
             var $usageProgressmeter = this.query("#usageProgressmeter");
             var $currentFiles = this.query("#currentFiles");
 
+            var $importUsage = this.query("#actionUsage");
+            var $importFiles = this.query("#actionFiles");
+            var $importStatus = this.query("#actionStatus");
+            
             var fileCount = this.imagefilesView.fileCount;
 
-            files.forEach(function(file) {
-                if (this.imagefilesView._totalSize <= this._disklimit) {
+            var importUsage = 0;
+            var importFiles = 0;
 
-                    file.copyTo(orgDir, "");
+            var result = GREUtils.Dialog.confirm(this.window, "Confirm Import",
+                                                              "Please attach the USB thumb drive containing the images to import and press OK to start the import.");
 
-                    this.imagefilesView._totalSize += file.fileSize;
-                    $currentUsage.val(this.Number.toReadableSize(this.imagefilesView._totalSize));
-                    $totalLimit.val(this.Number.toReadableSize(this._disklimit));
-                    var percent = Math.ceil(this.imagefilesView._totalSize / this._disklimit *100 );
-                    $usageProgressmeter.val(percent);
+            if (!result) return;
+            
+            $importStatus.val('in progress');
+            try {
+                files.forEach(function(file) {
+                    if (this.imagefilesView._totalSize <= this._disklimit) {
 
-                    var fileCountDir = new GeckoJS.Dir.readDir(this._dir);
-                    if (fileCountDir) fileCount = fileCountDir.length;
-                    $currentFiles.val(this.Number.format(fileCount));
-                }
+                        file.copyTo(orgDir, "");
 
-            }, this);
+                        this.imagefilesView._totalSize += file.fileSize;
+
+                        $currentUsage.val(this.Number.toReadableSize(this.imagefilesView._totalSize));
+                        $totalLimit.val(this.Number.toReadableSize(this._disklimit));
+                        var percent = Math.ceil(this.imagefilesView._totalSize / this._disklimit *100 );
+                        $usageProgressmeter.val(percent);
+
+                        var fileCountDir = new GeckoJS.Dir.readDir(this._dir);
+                        if (fileCountDir) fileCount = fileCountDir.length;
+                        $currentFiles.val(this.Number.format(fileCount));
+
+                        var totalSize = 0;
+                        fileCountDir.forEach(function(file) {
+                            totalSize += file.fileSize;
+                        });
+                        this.imagefilesView._totalSize = totalSize;
+
+                        importUsage += file.fileSize;
+                        importFiles++;
+                    }
+                }, this);
+                $importStatus.val('import done');
+            }
+            catch (e) {
+                $importStatus.val('error');
+            }
+
+            $importUsage.val(this.Number.toReadableSize(importUsage));
+            $importFiles.val(this.Number.format(importFiles));
+
             this.loadImage(this._dir);
 
         },
 
-        exportToDir: function() {
+        exportToDir: function(exportDir) {
+            if (!exportDir || (exportDir.length == 0)) exportDir = this._exportDir;
+
+            alert(this._dir + ':' + exportDir);
             
+            var result = GREUtils.Dialog.confirm(this.window, "Confirm Export",
+                                                              "Please attach the USB thumb drive to export images to and press OK to start the export.");
+
+            if (!result) return;
+
+            var $exportUsage = this.query("#actionUsage");
+            var $exportFiles = this.query("#actionFiles");
+            var $exportStatus = this.query("#actionStatus");
+
+            var fileCount = this.imagefilesView.fileCount;
+
+            var exportUsage = 0;
+            var exportFiles = 0;
+
+            if (!exportDir || (exportDir.length == 0)) exportDir = this._exportDir;
+            $exportStatus.val('in progress');
+            try {
+                var files = new GeckoJS.Dir.readDir(this._dir);
+
+                if (!GREUtils.File.isDir(exportDir) || !GREUtils.File.isWritable(exportDir)) {
+                    throw new Exception();
+                }
+
+                var destDir = GREUtils.File.getFile(exportDir);
+
+                files.forEach(function(file) {
+                    file.copyTo(destDir, "");
+                    exportUsage += file.fileSize;
+                    exportFiles++;
+                }, this);
+                $exportStatus.val('export done');
+            }
+            catch (e) {
+                $exportStatus.val('error');
+            }
+            $exportUsage.val(this.Number.toReadableSize(exportUsage));
+            $exportFiles.val(this.Number.format(exportFiles));
         },
 
         deleteImage: function() {
+            if (this._selectedFile == null) return;
 
             var result = GREUtils.Dialog.confirm(this.window, "Are you sure", "Are you sure want to delete '" + this._selectedFile.leafName + "'");
             if (result) {
@@ -148,6 +224,7 @@ var ImageFilesView = window.ImageFilesView = GeckoJS.NSITreeViewArray.extend({
         },
         
         renameImage: function() {
+            if (this._selectedFile == null) return;
 
             var input = {value: this._selectedFile.leafName};
             var result = GREUtils.Dialog.prompt(this.window, "Rename image", "Original image: '" + this._selectedFile.leafName, input);
