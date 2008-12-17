bcc78e503fe6cdb214e2b7f47e01b514a26ab1b9
AChang Hsiao <achangh@gmail.com>
2008-12-17 12:28:03 +0800

    vivipos app

---
+++ content/creditcard_remark.js
@@ -0,0 +1,68 @@
+(function(){
+
+    // include controllers  and register itself
+    GeckoJS.include('chrome://viviecr/content/controllers/plusearch_controller.js');
+
+    /**
+     * Controller Startup
+     */
+    function startup() {
+
+        centerWindowOnScreen();
+
+        $('#clearBtn')[0].addEventListener('command', clearTextBox, false);
+        // $('#ekeyBtn')[0].addEventListener('command', searchPlu, true);
+        $('#plu').focus();
+
+        doSetOKCancel(
+            function(){
+                // inputObj.condiments = document.getElementById('condiments').value;
+                //inputObj.index = document.getElementById('queueScrollablepanel').value;
+                // inputObj.ok = true;
+
+                return true;
+            },
+            function(){
+                // inputObj.ok = false;
+
+                return true;
+            }
+            );
+    };
+
+    /**
+     * Clear  box
+     */
+    function clearTextBox() {
+
+        $('#plu').val('');
+
+    };
+
+    function searchPlu () {
+            var barcode = $('#plu').val();
+            $('#plu').val('').focus();
+
+            if (barcode == "") return;
+
+            var productsById = GeckoJS.Session.get('productsById');
+            var barcodesIndexes = GeckoJS.Session.get('barcodesIndexes');
+            var product;
+
+            if (!barcodesIndexes[barcode]) {
+                // barcode notfound
+                // event.error = true;
+                // alert("Plu Not Found!");
+            }else {
+                var id = barcodesIndexes[barcode];
+                product = productsById[id];
+                GeckoJS.FormHelper.unserializeFromObject('productForm', product);
+
+            }
+        };
+
+    window.addEventListener('load', startup, false);
+
+})();
+
+
