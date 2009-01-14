20f96ba8f2e316b6de7c3097e3ecb8fa83a49f83
user <user@ubuntu804desktop.localdomain>
2009-01-14 18:12:27 +0800

    commit

--- defaults/preferences/database.js
+++ defaults/preferences/database.js
@@ -5,14 +5,14 @@ pref("DATABASE_CONFIG.json.classname", 'JsonFile');
 pref("DATABASE_CONFIG.json.path", '/var/tmp');
 
 pref("DATABASE_CONFIG.default.classname", 'SQLite');
-pref("DATABASE_CONFIG.default.path", '/var/tmp');
+pref("DATABASE_CONFIG.default.path", '/data/databases');
 pref("DATABASE_CONFIG.default.database", 'vivipos.sqlite');
 
 pref("DATABASE_CONFIG.acl.classname", 'SQLite');
-pref("DATABASE_CONFIG.acl.path", '/var/tmp');
+pref("DATABASE_CONFIG.acl.path", '/data/databases');
 pref("DATABASE_CONFIG.acl.database", 'vivipos_acl.sqlite');
 
 pref("DATABASE_CONFIG.order.classname", 'SQLite');
-pref("DATABASE_CONFIG.order.path", '/var/tmp');
+pref("DATABASE_CONFIG.order.path", '/data/databases');
 pref("DATABASE_CONFIG.order.database", 'vivipos_order.sqlite');
 
