60d01f585934a71c99a22ec3bbbf217a4de13395
Rack Lin <racklin@gmail.com>
2009-01-23 18:12:42 +0800

    update prefs

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -14,7 +14,7 @@ pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", false);
 pref("javascript.options.strict", false);
 pref("nglayout.debug.disable_xul_cache", false);
-pref("nglayout.debug.disable_xul_fastload", false);
+pref("nglayout.debug.disable_xul_fastload", true);
 
 //
 // Advanced DOM JavaScript preferences
