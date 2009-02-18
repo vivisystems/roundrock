886589d6e0ffabb3569d826cfa0b8cf1e2c7b477
AChang Hsiao <achangh@gmail.com>
2009-02-18 16:52:11 +0800

    modify reports..

---
+++ content/reports/controllers/components/check_media.js
@@ -0,0 +1,74 @@
+(function() {
+
+    var CheckMediaComponent = window.CheckMediaComponent = GeckoJS.Component.extend({
+
+    /**
+     * Component CheckMedia
+     */
+
+        name: 'CheckMedia',
+
+        initial: function () {
+            // @todo :
+            alert('check Media initial...');
+        },
+
+        // @todo
+        // folderName can not include the "/" at this moment...
+        checkMedia: function(folderName, autoCreate) {
+
+            // var osLastMedia = new GeckoJS.File('/tmp/last_media');
+            var osLastMedia = new GeckoJS.File('/var/tmp/vivipos/last_media');
+
+            var last_media = "";
+            var deviceNode = "";
+            var deviceReady = false;
+            this._backupDir = null;
+
+            // var deviceMount = "/media/";
+            var deviceMount = "/var/tmp/";
+
+            var hasMounted = false;
+
+            // auto create folder default is true...
+            if (typeof autoCreate == "undefine") autoCreate = true;
+
+            if (osLastMedia.exists()) {
+                osLastMedia.open("r");
+                last_media = osLastMedia.readLine();
+                osLastMedia.close();
+            }
+
+            if (last_media) {
+
+                var tmp = last_media.split('/');
+                deviceNode = tmp[tmp.length-1];
+                deviceMount +=  deviceNode + '/';
+
+
+
+                var mountDir = new GeckoJS.File(deviceMount);
+
+                if (mountDir.exists() && mountDir.isDir()) {
+
+                    // mount dir exists
+                    // autocreate given folder name
+                    var mediaDir = new GeckoJS.Dir(deviceMount + folderName, autoCreate);
+
+                    if (mediaDir.exists()) {
+
+                        // return target path if device ready...
+                        deviceReady = mediaDir.path;
+
+                        // deviceReady = true;
+
+                    }
+                }
+            }
+
+            return deviceReady ;
+
+        }
+    });
+
+})();
