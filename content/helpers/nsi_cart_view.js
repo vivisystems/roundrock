bef437028410ea75b121956b5b0023d043dca7c4
Irving Hsu <irving.hsu@firich.com.tw>
2009-02-19 08:24:05 +0800

    Fixed bug in cart_view rowCountChanged

--- content/helpers/nsi_cart_view.js
+++ content/helpers/nsi_cart_view.js
@@ -59,6 +59,7 @@
             }else {
                 this.tree.invalidate();
             }*/
+            //GREUtils.log('rowCountChanged 1: ' + oldIndex + ', ' + rc1 + ', ' + rc2 + ', ' + newIndex);
             if (rc1 < 0) rc1 = 0;
             if (rc2 < 0) rc2 = this.data.length;
             if (newIndex == null) newIndex = this.data.length - 1;
@@ -66,7 +67,7 @@
             if (rc1 != rc2) {
                 // lazy way ? full refresh
                 //this._cartList.datasource = this;
-                this.tree.rowCountChanged(this.tree.view.selection.current - 1, rc2 - rc1);
+                this.tree.rowCountChanged(oldIndex - 1, rc2 - rc1);
 
                 /*
                 if (jumpToLast) newIndex = rc2 - 1;
@@ -83,6 +84,7 @@
             }
             if (newIndex < 0) newIndex = (this.data.length > 0) ? 0 : -1;
             else if (newIndex >= this.data.length) newIndex = this.data.length - 1;
+            //GREUtils.log('rowCountChanged 2: ' + rc1 + ', ' + rc2 + ', ' + newIndex);
 
             this.tree.view.selection.currentIndex = newIndex;
             this.tree.view.selection.select(newIndex);
