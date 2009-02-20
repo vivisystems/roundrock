81f5ba3e39273190d901fe890ae280164e1de7ba
user <user@ubuntu804desktop.localdomain>
2009-02-20 12:40:37 +0800

    Changed tm88-iv command code for double height/double width

--- content/helpers/nsi_plus_view.js
+++ content/helpers/nsi_plus_view.js
@@ -110,6 +110,15 @@
                 //$(btn).addClass('font-'+ buttonFontSize);
             }
 
+            // is this product unavailable?
+            var unavailable = this.getCellValue(row,{
+                id: 'unavailable'
+            });
+
+            if (unavailable) {
+                btn.disabled = true;
+            }
+
             // display icon only?
             var icon_only = this.getCellValue(row,{
                 id: 'icon_only'
