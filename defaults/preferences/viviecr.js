761df96c6ca6d71ce2629784de3254ecb7c78b02
Rack Lin <racklin@gmail.com>
2009-03-31 14:45:04 +0800

    fixed prefs.js error

--- defaults/preferences/viviecr.js
+++ defaults/preferences/viviecr.js
@@ -49,11 +49,10 @@ pref("vivipos.fec.settings.controlpanels.config.sync_settings.path", "chrome://v
 pref("vivipos.fec.settings.controlpanels.config.sync_settings.icon", "chrome://viviecr/skin/icons/icon_sync.png");
 pref("vivipos.fec.settings.controlpanels.config.sync_settings.roles", "acl_manage_sync_settings");
 
-vipos.fec.settings.controlpanels.config.remote_control.label", "Remote Control");
+pref("vivipos.fec.settings.controlpanels.config.remote_control.label", "Remote Control");
 pref("vivipos.fec.settings.controlpanels.config.remote_control.path", "chrome://viviecr/content/remote_control.xul");
 pref("vivipos.fec.settings.controlpanels.config.remote_control.icon", "chrome://viviecr/skin/icons/icon_vnc.png");
 pref("vivipos.fec.settings.controlpanels.config.remote_control.roles", "acl_remote_control");
-e
 
 pref("vivipos.fec.settings.controlpanels.product.departments.label", "Departments");
 pref("vivipos.fec.settings.controlpanels.product.departments.path", "chrome://viviecr/content/departments.xul");
