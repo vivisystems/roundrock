4537e573c3bb15faff2f6589067b277fab3c44c9
user <user@ubuntu804desktop.localdomain>
2009-04-08 10:26:26 +0800

    Fixed bug where data.annotation leads to error when printing reports (because data is null)

--- content/controllers/print_controller.js
+++ content/controllers/print_controller.js
@@ -737,11 +737,11 @@
                 data.customer = GeckoJS.Session.get('current_customer');
                 data.store = GeckoJS.Session.get('storeContact');
                 if (data.store) data.store.terminal_no = GeckoJS.Session.get('terminal_no');
+                
+                // expand data with annotations
+                var annotationModel = new OrderAnnotationModel();
+                data.annotations = annotationModel.findByIndex('all', {index: 'order_id', value: data.order.id, order: 'type', recursive: 0});
 	        }
-
-            // expand data with annotations
-            var annotationModel = new OrderAnnotationModel();
-            data.annotations = annotationModel.findByIndex('all', {index: 'order_id', value: data.order.id, order: 'type', recursive: 0});
 
             // dispatch beforePrintCheck event to allow extensions to add to the template data object or
             // to prevent check from printed
