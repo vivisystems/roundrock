bae5ccd0711b28dddc2a6858ad52dbc97f8bccc0
Rack Lin <racklin@gmail.com>
2009-04-22 16:57:49 +0800

    fix report panel pageup/pagedown bug

--- content/reportPanel.js
+++ content/reportPanel.js
@@ -1,18 +1,15 @@
 (function(){
 
- /**
-  * Window Startup
-  */
+    var width = 0 , height = 0;
+    
+    /**
+     * Window Startup
+     */
     function startup() {
 
-        var width = GeckoJS.Configure.read("vivipos.fec.mainscreen.width") || 800;
-        var height = GeckoJS.Configure.read("vivipos.fec.mainscreen.height") || 600;
-
-        var openModel = function(url, name, args) {
-            var features = "chrome,titlebar,toolbar,centerscreen,modal,width=" + width + ",height=" + height;
-            return window.openDialog(url, name, features, args);
-        };
-    
+        width = GeckoJS.Configure.read("vivipos.fec.mainscreen.width") || 800;
+        height = GeckoJS.Configure.read("vivipos.fec.mainscreen.height") || 600;
+   
         centerWindowOnScreen();
 
         var prefs = GeckoJS.BaseObject.getValues(GeckoJS.Configure.read('vivipos.fec.reportpanels'));
@@ -20,29 +17,37 @@
         // if (data) data.forEach(function(el) {el.label = _(el.label)});
 
         if (data) data = data.map(function(el) {
-                var entry = {icon: el.icon,
-                             path: el.path,
-                             roles: el.roles,
-                             label: _(el.label)};
-                return entry;
-            });
+            var entry = {
+                icon: el.icon,
+                path: el.path,
+                roles: el.roles,
+                label: _(el.label)
+                };
+            return entry;
+        });
 
         window.viewHelper = new opener.GeckoJS.NSITreeViewArray(data);
 
         document.getElementById('imagePanel').datasource = window.viewHelper;
 
-        document.getElementById('imagePanel').addEventListener('command', function(evt) {
-
-            var index = evt.target.currentIndex;
-            var pref = data[index];
-            var aArguments = "";
-
-            $('#loading').show();
-            openModel(pref['path'], "Preferences_" + pref['label'], aArguments);
-            $('#loading').hide();
-        }, true);
-
     }
+
+    window.openModel = function openModel(url, name, args) {
+        var features = "chrome,titlebar,toolbar,centerscreen,modal,width=" + width + ",height=" + height;
+        return window.openDialog(url, name, features, args);
+    };
+
+
+    window.launchReport =  function launchReport (panel) {
+        var data = panel.datasource.data;
+        var index = panel.currentIndex;
+        var pref = data[index];
+        var aArguments = "";
+
+        $('#loading').show();
+        openModel(pref['path'], "Preferences_" + pref['label'], aArguments);
+        $('#loading').hide();
+    };
 
     window.addEventListener('load', startup, true);
 
