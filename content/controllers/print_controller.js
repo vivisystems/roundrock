abadf27815d70fe1a0436e72c21a39797f4d0a52
user <user@ubuntu804desktop.localdomain>
2009-05-06 11:11:52 +0800

    Uses isoptionenabled to turn on/off clockin

--- content/controllers/print_controller.js
+++ content/controllers/print_controller.js
@@ -910,7 +910,7 @@
             }
             //@debug
             //alert(GeckoJS.BaseObject.dump(result));
-            this.log(GeckoJS.BaseObject.dump(result));
+            //this.log(GeckoJS.BaseObject.dump(result));
             //return;
             //alert(data.order.receiptPages);
             //
