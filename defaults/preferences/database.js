b1b12918b448fb15b813ba86b9b02b65cfc1647a
Rack Lin <racklin@gmail.com>
2009-05-22 17:26:06 +0800

    add database timeout prefs , ex: DATABASE_CONFIG.order.timeout

--- defaults/preferences/database.js
+++ defaults/preferences/database.js
@@ -7,18 +7,22 @@ pref("DATABASE_CONFIG.json.path", '/var/tmp');
 pref("DATABASE_CONFIG.default.classname", 'SQLite');
 pref("DATABASE_CONFIG.default.path", '/data/databases');
 pref("DATABASE_CONFIG.default.database", 'vivipos.sqlite');
+pref("DATABASE_CONFIG.default.timeout", '30');
 
 pref("DATABASE_CONFIG.acl.classname", 'SQLite');
 pref("DATABASE_CONFIG.acl.path", '/data/databases');
 pref("DATABASE_CONFIG.acl.database", 'vivipos_acl.sqlite');
+pref("DATABASE_CONFIG.acl.timeout", '30');
 
 pref("DATABASE_CONFIG.order.classname", 'SQLite');
 pref("DATABASE_CONFIG.order.path", '/data/databases');
 pref("DATABASE_CONFIG.order.database", 'vivipos_order.sqlite');
+pref("DATABASE_CONFIG.order.timeout", '30');
 
 pref("DATABASE_CONFIG.extension.classname", 'SQLite');
 pref("DATABASE_CONFIG.extension.path", '/data/databases');
 pref("DATABASE_CONFIG.extension.database", 'vivipos_extension.sqlite');
+pref("DATABASE_CONFIG.extension.timeout", '30');
 
 pref("DATABASE_CONFIG.memory.classname", 'SQLite');
 pref("DATABASE_CONFIG.memory.path", '');
