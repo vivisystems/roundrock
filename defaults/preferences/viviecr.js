8fb0a2b1d95ba63cff8cc21719b9b2ed55e98662
Mickey <mickey@mickey.(none)>
2009-05-26 20:37:26 +0800

    Mickey developed a new table called 'customer transaction report'.

--- defaults/preferences/viviecr.js
+++ defaults/preferences/viviecr.js
@@ -678,6 +678,11 @@ pref("vivipos.fec.reportpanels.promotionsummary.path", "chrome://viviecr/content
 pref("vivipos.fec.reportpanels.promotionsummary.icon", "chrome://viviecr/content/skin/icons/editcopy.png");
 pref("vivipos.fec.reportpanels.promotionsummary.roles", "acl_run_report");
 
+pref("vivipos.fec.reportpanels.customertransaction.label", "Customer Transaction Report");
+pref("vivipos.fec.reportpanels.customertransaction.path", "chrome://viviecr/content/reports/rpt_customer_transaction.xul");
+pref("vivipos.fec.reportpanels.customertransaction.icon", "chrome://viviecr/content/skin/icons/editcopy.png");
+pref("vivipos.fec.reportpanels.customertransaction.roles", "acl_run_report");
+
 pref("vivipos.fec.registry.templates.receipt-sample-condensed-42c.label", "Sample Receipt 42c condensed");
 pref("vivipos.fec.registry.templates.receipt-sample-condensed-42c.type", "receipt");
 pref("vivipos.fec.registry.templates.receipt-sample-condensed-42c.path", "chrome://viviecr/content/devices/templates/receipt-sample-condensed-42c.tpl");
