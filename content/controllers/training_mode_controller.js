0226b6d9176f7d1a77b97f439b24a03966b85dc9
Rack Lin <racklin@gmail.com>
2009-06-11 15:37:49 +0800

    disable sync when enter training mode

--- content/controllers/training_mode_controller.js
+++ content/controllers/training_mode_controller.js
@@ -1,17 +1,18 @@
 ( function() {
-	/**
+    /**
 	 * Class TrainingMode
 	 */
 	 
-	 var __controller__ = {
-	 	name: "TrainingMode",
+    var __controller__ = {
+        name: "TrainingMode",
 	 	
-	 	_orderDBConfig: "DATABASE_CONFIG.order",
-	 	_trainingOrderDBConfig: "DATABASE_CONFIG.training_order",
-	 	_emptyTrainingOrderDBConfg: "DATABASE_CONFIG.empty_training_order",
-	 	_defaultTrainingOrderDBConfg: "DATABASE_CONFIG.default_training_order",
-	 	
-	 	execute: function( cmd, param ) {
+        _orderDBConfig: "DATABASE_CONFIG.order",
+        _trainingOrderDBConfig: "DATABASE_CONFIG.training_order",
+        _emptyTrainingOrderDBConfg: "DATABASE_CONFIG.empty_training_order",
+        _defaultTrainingOrderDBConfg: "DATABASE_CONFIG.default_training_order",
+        _origSyncActive: null,
+
+        execute: function( cmd, param ) {
             try {
                 var exec = new GeckoJS.File( cmd );
                 var r = exec.run( param, true );
@@ -32,15 +33,15 @@
          */
         _copyToReplaceDB: function( dbToReplace, dbToBeReplaced ) {
             var confDBToReplace =
-        		GeckoJS.Configure.read( dbToReplace );
-        	var confDBToBeReplaced =
-        		GeckoJS.Configure.read( dbToBeReplaced );
-        	var pathDBToReplace =
-        		confDBToReplace.path + '/' + confDBToReplace.database;
-        	var pathDBToBeReplaced =
-        		confDBToBeReplaced.path + '/' + confDBToBeReplaced.database;
+            GeckoJS.Configure.read( dbToReplace );
+            var confDBToBeReplaced =
+            GeckoJS.Configure.read( dbToBeReplaced );
+            var pathDBToReplace =
+            confDBToReplace.path + '/' + confDBToReplace.database;
+            var pathDBToBeReplaced =
+            confDBToBeReplaced.path + '/' + confDBToBeReplaced.database;
         	
-        	this.execute( "/bin/cp", [ pathDBToReplace, pathDBToBeReplaced ] );
+            this.execute( "/bin/cp", [ pathDBToReplace, pathDBToBeReplaced ] );
         },
         
         _vacuumTrainingDB: function() {
@@ -48,7 +49,7 @@
         },
         
         vacuumTrainingDB: function() {
-        	if ( !GREUtils.Dialog.confirm( window, '', _( 'Are you sure you want to VACUUM training database?' ) ) )
+            if ( !GREUtils.Dialog.confirm( window, '', _( 'Are you sure you want to VACUUM training database?' ) ) )
                 return;
             this._vacuumTrainingDB();
         },
@@ -58,9 +59,9 @@
         },
         
         takeCurrentDBToBeDefaultDB: function() {
-        	if ( !GREUtils.Dialog.confirm( window, '', _( 'Are you sure you want to take current database to be default database?' ) ) )
+            if ( !GREUtils.Dialog.confirm( window, '', _( 'Are you sure you want to take current database to be default database?' ) ) )
                 return;
-        	this._takeCurrentDBToBeDefaultDB();
+            this._takeCurrentDBToBeDefaultDB();
         },
         
         _takeDefaultDBToBeTrainingDB: function() {
@@ -68,47 +69,76 @@
         },
         
         takeDefaultDBToBeTrainingDB: function() {
-        	if ( !GREUtils.Dialog.confirm( window, '', _( 'Are you sure you want to take default database to be training database?' ) ) )
+            if ( !GREUtils.Dialog.confirm( window, '', _( 'Are you sure you want to take default database to be training database?' ) ) )
                 return;
-        	this._takeDefaultDBToBeTrainingDB();
+            this._takeDefaultDBToBeTrainingDB();
         },
-	 	
-	 	start: function() {
-	 		var isTraining = GeckoJS.Session.get( "isTraining" );
+
+        enableSyncActive: function() {
+
+            // setting global SyncSetting
+            if(SyncSetting._setting && this._origSyncActive == 1) {
+                SyncSetting._setting.active = 1;
+            }
+
+
+        },
+
+        disableSyncActive: function() {
+
+            // setting global SyncSetting
+            if(SyncSetting._setting && SyncSetting._setting.active == 1) {
+                SyncSetting._setting.active = 0;
+            }
+            
+        },
+
+        start: function() {
+            var isTraining = GeckoJS.Session.get( "isTraining" );
 	 		
-	 		var trainingModeController = GeckoJS.Controller.getInstanceByName( "TrainingMode" );
+            var trainingModeController = GeckoJS.Controller.getInstanceByName( "TrainingMode" );
 	 		
-	 		if ( isTraining ) {
-	 			if ( GREUtils.Dialog.confirm( window, '', _( 'Are you going to leave the training mode?' ) ) ) {
-	 				GeckoJS.Session.set( "isTraining", 0 );
+            if ( isTraining ) {
+                if ( GREUtils.Dialog.confirm( window, '', _( 'Are you going to leave the training mode?' ) ) ) {
+                    GeckoJS.Session.set( "isTraining", 0 );
 	 				
-	 				//Vacuum the training DB by just substituting an empty one for it.
-	 				//this.vacuumTrainingDB();
+                    //Vacuum the training DB by just substituting an empty one for it.
+                    //this.vacuumTrainingDB();
 	 				
-	 				//trainingModeController.dispatchEvent( "startTrainingMode", "exit" );
-	 				GeckoJS.Observer.notify( null, "TrainingMode", "exit" );
-	 			}
-	 		} else {
-	 			var cart = GeckoJS.Controller.getInstanceByName( "Cart" );
+                    //trainingModeController.dispatchEvent( "startTrainingMode", "exit" );
+                    GeckoJS.Observer.notify( null, "TrainingMode", "exit" );
+
+                    // enableSyncActive
+                    this.enableSyncActive();
+                }
+            } else {
+                var cart = GeckoJS.Controller.getInstanceByName( "Cart" );
 	 			
-	 			if ( cart.ifHavingOpenedOrder() ) {
-	 				alert( _( "The training mode can be launched only if there is no opened order." ) );
-	 				return;
-		 		}
+                if ( cart.ifHavingOpenedOrder() ) {
+                    alert( _( "The training mode can be launched only if there is no opened order." ) );
+                    return;
+                }
 	 			
-	 			if ( GREUtils.Dialog.confirm( window, '', _( "Are you sure you want to start training?" ) ) ) {
-			 		// set up the flag indicating that we are now in the training mode.
-			 		GeckoJS.Session.add( "isTraining", 1 );
+                if ( GREUtils.Dialog.confirm( window, '', _( "Are you sure you want to start training?" ) ) ) {
+                    // set up the flag indicating that we are now in the training mode.
+                    GeckoJS.Session.add( "isTraining", 1 );
+
 			 		
-			 		//Vacuum the training DB by just substituting an empty one for it.
-	 				//this.vacuumTrainingDB();
+                    //Vacuum the training DB by just substituting an empty one for it.
+                    //this.vacuumTrainingDB();
 			 		
-			 		//trainingModeController.dispatchEvent( "startTrainingMode", "start" );
-			 		GeckoJS.Observer.notify( null, "TrainingMode", "start" );
-			 	}
-		 	}
-	 	}
-	 };
+                    //trainingModeController.dispatchEvent( "startTrainingMode", "start" );
+                    GeckoJS.Observer.notify( null, "TrainingMode", "start" );
+
+                    this._origSyncActive = ((new SyncSetting()).read()).active;
+                    
+                    // disableSyncActive
+                    this.disableSyncActive();
+
+                }
+            }
+        }
+    };
 	 
-	 GeckoJS.Controller.extend( __controller__ );
+    GeckoJS.Controller.extend( __controller__ );
 } )();
