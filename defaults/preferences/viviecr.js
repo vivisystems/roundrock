1922a12408a0d6acdae11d3efa36c1ab06ed495d
Mickey <mickey@mickey.(none)>
2009-06-11 09:41:33 +0800

    Mickey moved the configuration of the database files for the use of training funciton to the preference file along with vivipos_app.

--- defaults/preferences/viviecr.js
+++ defaults/preferences/viviecr.js
@@ -3,6 +3,16 @@ pref("DATABASE_CONFIG.table.classname", 'SQLite');
 pref("DATABASE_CONFIG.table.path", '/data/databases');
 pref("DATABASE_CONFIG.table.database", 'vivipos_table.sqlite');
 
+pref("DATABASE_CONFIG.training_order.classname", 'SQLite');
+pref("DATABASE_CONFIG.training_order.path", '/data/training');
+pref("DATABASE_CONFIG.training_order.database", 'vivipos_order.sqlite');
+pref("DATABASE_CONFIG.training_order.timeout", '15');
+
+pref("DATABASE_CONFIG.empty_training_order.classname", 'SQLite');
+pref("DATABASE_CONFIG.empty_training_order.path", '/data/training');
+pref("DATABASE_CONFIG.empty_training_order.database", 'empty_vivipos_order.sqlite');
+pref("DATABASE_CONFIG.empty_training_order.timeout", '15');
+
 /* extension vivipos */
 pref("vivipos.fec.firstrun", false);
 pref("vivipos.fec.settings.DateFormatStr", "yyyy-mm-dd");
