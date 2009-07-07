1bea830a838b4017963b4bb5e22b296473d2a030
Mickey Ju <MickeyJu@gmail.com>
2009-07-07 11:28:41 +0800

    Mickey modified the stock report.

--- content/reports/controllers/rpt_stocks_controller.js
+++ content/reports/controllers/rpt_stocks_controller.js
@@ -11,17 +11,21 @@
         
         _fileName: "rpt_stocks",
 
-        _set_reportRecords: function(limit) {
+        _set_reportRecords: function( limit ) {
 
-            limit = parseInt(limit);
-            if (isNaN(limit) || limit <= 0) limit = this._stdLimit;
+            limit = parseInt( limit );
+            if ( isNaN( limit ) || limit <= 0 ) limit = this._stdLimit;
 
-            var department = document.getElementById('department').value;
+            var department = document.getElementById( 'department' ).value;
             var sortby = document.getElementById( 'sortby' ).value;
-
-            var fields = [ 'cate_no', 'no', 'name', 'stock', 'min_stock' ];
-
-            var conditions = null;
+            var show_low_stock = document.getElementById( 'show_low_stock' ).checked;
+            var show_maintained_ones = document.getElementById( 'show_maintained_ones' ).checked;
+            
+            var conditions = "1 = 1";
+            if ( show_low_stock == true )
+                conditions += " AND s.quantity < p.min_stock";
+            if ( show_maintained_ones == true )
+                conditions += " AND p.auto_maintain_stock = 1"
 
             if (department != "all") {
                 var cate = new CategoryModel();
@@ -39,53 +43,68 @@
             }
             
             var records = [];
-            cateRecords.forEach(function(o){
-                records[o.no] = {
-                    no:o.no,
-                    name:o.name
-                    };
-            });
+            cateRecords.forEach( function( o ) {
+                records[ o.no ] = {
+                    no: o.no,
+                    name: o.name,
+                    plu: []
+                };
+            } );
 
             var groupby;
             
-            var orderby = 'products.cate_no';
+            var orderby = 'p.cate_no';
             
             if ( sortby != 'all' )
                 switch(sortby) {
                     case 'no':
                     case 'name':
-                        orderby += ',products.' + sortby;
+                        orderby += ', p.' + sortby;
                         break;
                     case 'stock':
                     case 'min_stock':
-                        orderby += ',products.' + sortby + ' DESC';
+                        orderby += ', p.' + sortby + ' DESC';
                         break;
                 }
 
+            var fields = [
+                'p.cate_no',
+                'p.no',
+                'p.name',
+                'p.min_stock',
+                'p.auto_maintain_stock',
+                's.quantity'
+            ];
+            
+            var sql =
+                "SELECT " + fields.join( ", " ) + " FROM products p LEFT JOIN stock_records s ON ( p.no = s.product_no ) " +
+                "WHERE " + conditions + " ORDER BY " + orderby + " LIMIT " + this._csvLimit + ";";
+            
             var prod = new ProductModel();
-            var prodRecords = prod.find('all', { fields: fields, conditions: conditions, order: orderby, limit: this._csvLimit });
+            //var prodRecords = prod.find( 'all', { fields: fields, order: orderby, limit: this._csvLimit } );
+            var prodRecords = prod.getDataSource().fetchAll( sql );
 
-            prodRecords.forEach(function(o){
-                if (!(o.cate_no in records) && department == 'all') {
-                    records[o.cate_no] = {
+            prodRecords.forEach( function( o ) {
+                if ( !( o.cate_no in records ) && department == 'all' ) {
+                    records[ o.cate_no ] = {
                         no: o.cate_no,
-                        name: _('(rpt)Obsolete Department')
+                        name: _( '(rpt)Obsolete Department' )
                     };
                 }
-                if (records[o.cate_no]) {
-                    if (records[o.cate_no].plu == null) {
-                        records[o.cate_no].plu = [];
+                if ( records[ o.cate_no ] ) {
+                    if ( records[ o.cate_no ].plu == null ) {
+                        records[ o.cate_no ].plu = [];
                     }
                     records[o.cate_no].plu.push( {
                         cate_no:o.cate_no,
                         no:o.no,
                         name:o.name,
-                        stock:o.stock,
+                        stock:o.quantity,
                         min_stock:o.min_stock
-                        });
+                    } );
                 }
-            });
-
+            } );
+this.log( this.dump( records ) );
 			this._reportRecords.head.title = _( 'vivipos.fec.reportpanels.stocks.label' );
 			this._reportRecords.body = records;
         },
