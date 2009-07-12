65866c501d6d0e8669073c33f2253cff2edd3c3d
user <user@ubuntu804desktop.localdomain>
2009-07-13 05:45:23 +0800

    Imported updated XBLs from Aurora

--- defaults/preferences/resolution.js
+++ defaults/preferences/resolution.js
@@ -1,5 +1,5 @@
 // VIVIPOS resolutin prefs
-pref("general.skins.selectedSkin", "traditional-800x600/1.0");
+pref("general.skins.selectedSkin", "traditional-800x600.0");
 pref("vivipos.fec.mainscreen.width", 800);
 pref("vivipos.fec.mainscreen.height", 600);
 pref("vivipos.fec.mainscreen.hidechrome", false);
