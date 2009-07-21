4c6206e36d0c46ab9856bedd572cb39ff142f4f4
Rack Lin <racklin@gmail.com>
2009-07-21 13:43:28 +0800

    update version to 1.1.18: 	regenerated api docs 	databases add synchronous options

--- defaults/preferences/database.js
+++ defaults/preferences/database.js
@@ -8,7 +8,9 @@ pref("DATABASE_CONFIG.default.classname", 'SQLite');
 pref("DATABASE_CONFIG.default.path", '/data/databases');
 pref("DATABASE_CONFIG.default.database", 'vivipos.sqlite');
 pref("DATABASE_CONFIG.default.timeout", '30');
+pref("DATABASE_CONFIG.default.synchronous", 'NORMAL');
 
 pref("DATABASE_CONFIG.memory.classname", 'SQLite');
 pref("DATABASE_CONFIG.memory.path", '');
 pref("DATABASE_CONFIG.memory.database", ':in-memory');
+
