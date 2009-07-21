9b276eb50375ee921737a15d1644d3f73a70284c
Mickey Ju <MickeyJu@gmail.com>
2009-07-21 12:25:54 +0800

    Mickey added status selector and destination selector to your_order_report.

--- content/reports/controllers/rpt_your_order_controller.js
+++ content/reports/controllers/rpt_your_order_controller.js
@@ -50,6 +50,10 @@
             var periodType = document.getElementById( 'period_type' ).value;
             
             var sortby = document.getElementById( 'sortby' ).value;
+            
+            var status = document.getElementById( 'status' ).value;
+            
+            var destination = document.getElementById( 'destination' ).value;
 
             start = parseInt( start / 1000, 10 );
             end = parseInt( end / 1000, 10 );
@@ -66,14 +70,19 @@
             ];
 
             var conditions = "orders." + periodType + ">='" + start +
-                "' AND orders." + periodType + "<='" + end +
-                "' AND orders.status=1";
+                "' AND orders." + periodType + "<='" + end + "'";
                             
             if ( terminal_no.length > 0 )
                 conditions += " AND orders.terminal_no LIKE '" + this._queryStringPreprocessor( terminal_no ) + "%'";
                 
             if ( shiftNo.length > 0 )
                 conditions += " AND orders.shift_number = '" + this._queryStringPreprocessor( shiftNo ) + "'";
+                
+            if ( status.length > 0 && status != "all" )
+                conditions += " AND orders.status = '" + status + "'";
+                
+            if ( destination.length > 0 && destination != "all" )
+                conditions += " AND orders.destination = '" + destination + "'";
                 
             var groupby = 'orders.id, order_payments.name';
             var orderby = 'orders.' +  timeField;
@@ -515,6 +524,20 @@
             document.getElementById( 'start_date' ).value = start;
             document.getElementById( 'end_date' ).value = end;
             
+            // initialize the destination selector.
+            var destination_records = GeckoJS.Configure.read( "vivipos.fec.settings.Destinations" );
+            destination_records = GeckoJS.BaseObject.unserialize( GeckoJS.String.urlDecode( destination_records ) );
+            
+            var menupopup = document.getElementById( "destination_menupopup" );
+            
+            destination_records.forEach( function( record ) {
+                var menuitem = document.createElementNS( "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "xul:menuitem" );
+                menuitem.setAttribute( 'value', record.name );
+                menuitem.setAttribute( 'label', record.name );
+                menupopup.appendChild( menuitem );
+            } );
+            
+            // disable the textbox for width since we have nothing to adjust this time.
             document.getElementById( this._reportWidthTextBoxId ).disabled = true;
             
             // Get preferences from report.js for this report.
