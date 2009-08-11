d3fc346ef2050e2a88cb8ef7a300fca240a85eed
Rack Lin <racklin@gmail.com>
2009-08-11 15:05:18 +0800

    add Multi-Group / Multi-Dept promotions trigger

---
+++ content/promotions/controllers/multi_dept_trigger_setting_controller.js
@@ -0,0 +1,106 @@
+(function(){
+
+    // key is in prefs 's registry key
+    var key = 'multi_dept';
+    var sessionKey = "promotions_manager.trigger.settings."+key;
+
+    /**
+     * Controller Promotions Manager
+     * 
+     */
+    var __controller__ = {
+
+        name: 'MultiDeptTriggerSetting',
+
+        _pluGroups: [],
+        
+        /*
+         * initial promotions rules for register
+         */
+        initial: function() {
+
+            this.screenwidth = GeckoJS.Configure.read('vivipos.fec.mainscreen.width') || 800;
+            this.screenheight = GeckoJS.Configure.read('vivipos.fec.mainscreen.height') || 600;
+
+
+            this._deptscrollablepanel1 = document.getElementById('deptscrollablepanel1');
+            this._deptPanelView1 =  new NSICategoriesView('deptscrollablepanel1');
+
+            this._deptscrollablepanel2 = document.getElementById('deptscrollablepanel2');
+            this._deptPanelView2 =  new NSICategoriesView('deptscrollablepanel2');
+
+            var settings = GeckoJS.Session.get(sessionKey) || null;
+
+            this.setSettings(settings);
+
+        },
+
+        setSettings: function(settings) {
+            this.updateForm(settings);
+        },
+
+        getSettings: function() {
+
+            var formData = this.Form.serializeToObject('multiDeptForm');
+            
+            // set to sessions
+            var settings = formData;
+
+            var panelView1 = this._deptPanelView1;
+            var panelView2 = this._deptPanelView2;
+
+            var categories = GeckoJS.Session.get('categoriesById');
+
+            var firstCategory = categories[panelView1.getValue()];
+            var secondCategory = categories[panelView2.getValue()];
+
+            settings.first_id = firstCategory.id;
+            settings.first_no = firstCategory.no;
+            settings.second_id = secondCategory.id;
+            settings.second_no = secondCategory.no;
+
+            this.log(this.dump(settings));
+            
+            GeckoJS.Session.set(sessionKey, settings);
+
+        },
+
+        updateForm: function(item) {
+
+            if (item) {
+                
+                this.Form.unserializeFromObject('multiDeptForm', item);
+
+                this.updatePanelView(item);
+
+            }else {
+                this.Form.reset('multiDeptForm');
+            }
+
+        },
+
+        updatePanelView: function(item) {
+
+            var panelView1 = this._deptPanelView1;
+            var panelView2 = this._deptPanelView2;
+
+            panelView1.setValue(item.first_id);
+            panelView2.setValue(item.second_id);
+            
+        }
+
+    };
+
+    GeckoJS.Controller.extend(__controller__);
+
+
+    window.addEventListener('load', function() {
+        $do('initial', null, 'MultiDeptTriggerSetting');
+    }, false);
+
+    // when promotion rule modify
+    window.addEventListener('promotion_modify', function() {
+        $do('getSettings', null, 'MultiDeptTriggerSetting');
+    }, false);
+
+})();
