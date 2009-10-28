fb655f295f4d8a48044fde1743a975ad22469fa7
vivipos <irving.hsu@firich.com.tw>
2009-10-28 22:39:31 +0800

    Removed Bixolon printer from device.js

--- defaults/preferences/device.js
+++ defaults/preferences/device.js
@@ -105,11 +105,6 @@ pref("vivipos.fec.registry.devicemodels.epson-tm-t88iv.type", "receipt,check,rep
 pref("vivipos.fec.registry.devicemodels.epson-tm-t88iv.path", "chrome://viviecr/content/devices/commands/epson-tm88iv.cmd");
 pref("vivipos.fec.registry.devicemodels.epson-tm-t88iv.encodings", "English=US-ASCII,Western=ISO-8859-1,Simplified Chinese=GB18030,Simplified Chinese=GB2312,Traditional Chinese=Big5,UTF-8=UTF-8");
 
-pref("vivipos.fec.registry.devicemodels.bixolon-srp-350.label", "Bixolon SRP 350");
-pref("vivipos.fec.registry.devicemodels.bixolon-srp-350.type", "receipt,check,report,cashdrawer");
-pref("vivipos.fec.registry.devicemodels.bixolon-srp-350.path", "chrome://viviecr/content/devices/commands/bixolon_srp_350_plus.cmd");
-pref("vivipos.fec.registry.devicemodels.bixolon-srp-350.encodings", "English=US-ASCII,Western=ISO-8859-1,Simplified Chinese=GB18030,Simplified Chinese=GB2312,Traditional Chinese=Big5,UTF-8=UTF-8");
-
 pref("vivipos.fec.registry.devicemodels.epson-rp-u420.label", "Epson RP-U420");
 pref("vivipos.fec.registry.devicemodels.epson-rp-u420.type", "receipt,check");
 pref("vivipos.fec.registry.devicemodels.epson-rp-u420.path", "chrome://viviecr/content/devices/commands/epson-rp-u420.cmd");
