1de3be3954e76d04bf4f043b637d11da157ad7b4
vivipos <irving.hsu@firich.com.tw>
2009-12-01 11:50:43 +0800

    use new barcode command with template

--- content/label_printing.js
+++ content/label_printing.js
@@ -5,17 +5,11 @@
  */
 function startup() {
 
- //   $do('createGroupPanel', null, 'Tab');
     $do('createDepartmentPanel', null, 'Tab');
     $do('createIndividualDepartmentPanel', null, 'Tab');
     $do('loadCommitments', null, 'Tab');
- //   $do('initDefaultTax', null, 'Tab');
-
  //   $do('createFilterRows', null, 'PluSearch');
-
- //   $do('initSearchCallback', null, 'Tab');
     $do('load', null, 'Tab');
-
  //   $('#imageBrowseBtn')[0].addEventListener('command', selectImages, false);
  //   $('#imageRemoveBtn')[0].addEventListener('command', RemoveImage, false);
 
