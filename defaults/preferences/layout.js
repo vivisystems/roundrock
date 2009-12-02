d3ae33e672907a7865812cc6457c1b1827b2c9b6
user <wayne_hung_@hotmail.com>
2009-12-02 11:35:20 +0800

    tmp

--- defaults/preferences/layout.js
+++ defaults/preferences/layout.js
@@ -8,8 +8,8 @@ pref("vivipos.fec.registry.layouts.traditional.resolutions", "800x600,1024x768")
 pref("vivipos.fec.registry.layouts.traditional.disabled_features", "");
 pref("vivipos.fec.registry.layouts.traditional.skin", "traditional-${width}x${height}/1.0");
 
-pref("vivipos.fec.registry.layouts.jcm.label", "jcm");
-pref("vivipos.fec.registry.layouts.jcm.desc", "jcm");
+pref("vivipos.fec.registry.layouts.jcm.label", "chrome://viviecr/locale/messages.properties");
+pref("vivipos.fec.registry.layouts.jcm.desc", "chrome://viviecr/locale/messages.properties");
 pref("vivipos.fec.registry.layouts.jcm.overlay_uri", "chrome://viviecr/content/layouts/jcm/jcm.xul");
 pref("vivipos.fec.registry.layouts.jcm.icon", "chrome://viviecr/content/layouts/jcm/jcm.png");
 pref("vivipos.fec.registry.layouts.jcm.resolutions", "800x600,1024x768");
