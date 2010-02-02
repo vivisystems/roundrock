77377416d8d8cf7d948f0f6db55798135eac39d7
vivipos <irving.hsu@firich.com.tw>
2010-02-02 16:06:26 +0800

    Fixed bug #347
    Removed customerPanelContainer from traditional.xul

--- content/layouts/jcm/jcm_prefs.js
+++
@@ -1,7 +0,0 @@
-
-function prefs_overlay_startup() {
-    var prefwin = document.getElementById('prefwin');
-    var displayPane = document.getElementById('displaySettingsPane');
-
-    if (displayPane) prefwin.addPane(displayPane);
-}
