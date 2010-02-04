34ce48d8d4a8f55be7f74ee9f390051ea76f90eb
Rack Lin <racklin@gmail.com>
2010-02-04 11:22:21 +0800

    Add exclusive lock for model.

--- app/app_model.php
+++ app/app_model.php
@@ -7,14 +7,37 @@ class AppModel extends Model {
 
     function begin() {
         $db =& $this->getDataSource();
-        $db->begin($this);
+        return $db->begin($this);
     }
+
+    /**
+     * Begin Exclusive lock only for sqlite
+     * @return <type>
+     *
+     */
+    function beginExclusive() {
+
+        $db =& $this->getDataSource();
+
+        // check datasource is sqlite ??
+        if (!strcasecmp(get_class($db), 'sqlite')) {
+            return $this->begin();
+        }
+
+        if (!$db->_transactionStarted && $db->execute('BEGIN EXCLUSIVE')) {
+                $db->_transactionStarted = true;
+                return true;
+        }
+        return false;
+    }
+
 
     function commit() {
         $db =& $this->getDataSource();
         $db->commit($this);
     }
 
+    
     function rollback() {
         $db =& $this->getDataSource();
         $db->rollback($this);
