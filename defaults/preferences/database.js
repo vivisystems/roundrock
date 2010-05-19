afb6d27eb35e40ee48bb02741dd5f2d09e7915de
Rack Lin <racklin@gmail.com>
2010-05-19 16:18:04 +0800

    change journal_mode from MEMORY to TRUNCATE. and run_vivipos will touch *-journal and chmod 775 .
    [#13 state:committed]

--- defaults/preferences/database.js
+++ defaults/preferences/database.js
@@ -9,7 +9,7 @@ pref("DATABASE_CONFIG.default.path", '/data/databases');
 pref("DATABASE_CONFIG.default.database", 'vivipos.sqlite');
 pref("DATABASE_CONFIG.default.timeout", '30');
 pref("DATABASE_CONFIG.default.synchronous", 'NORMAL');
-pref("DATABASE_CONFIG.default.journal_mode", 'MEMORY');
+pref("DATABASE_CONFIG.default.journal_mode", 'TRUNCATE');
 
 pref("DATABASE_CONFIG.memory.classname", 'SQLite');
 pref("DATABASE_CONFIG.memory.path", '');
