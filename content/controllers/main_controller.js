f2d769512524cdce7f147b1f27c68eb65d022a56
Rack Lin <racklin@gmail.com>
2010-07-09 10:17:57 +0800

    Force savePrefs to prefs.js and user.js before shutdown or reboot machine.
    [ #112  state:committed]

--- content/controllers/main_controller.js
+++ content/controllers/main_controller.js
@@ -1593,6 +1593,7 @@
 
         shutdownMachine: function() {
             try {
+                this._flushPrefs();
                 goShutdownMachine();
             }catch(e) {
             }
@@ -1600,6 +1601,7 @@
 
         rebootMachine: function() {
             try {
+                this._flushPrefs();
                 goRebootMachine();
             }catch(e) {
             }
@@ -2110,8 +2112,21 @@
             }
 
             return null;
-        }
+        },
 
+
+        _flushPrefs: function() {
+            try {
+                var mPrefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+                mPrefService.savePrefFile(null);
+
+                var profD = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
+                var userJs = profD.clone(); userJs.append('user.js');
+                mPrefService.savePrefFile(userJs);
+            }catch(e) {
+                this.log('ERROR', 'Error save prefs.js and user.js');
+            }
+        }
 
     };
 
