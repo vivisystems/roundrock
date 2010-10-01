51d58782a4a5fb1a33bf625af480f7d663997b93
Rack Lin <racklin@gmail.com>
2010-10-01 14:18:06 +0800

    vivipos.log 's FileAppender will using prefs's  log level not WARN.
    [#30 state:committed responsible:frank]

--- chrome/content/vivipos.js
+++ chrome/content/vivipos.js
@@ -29,9 +29,10 @@
 
             // set log level
             var logLevel = GeckoJS.Configure.read('vivipos.fec.debug.log_level') || 'ERROR';
+            var logLevelObj = GeckoJS.Log[logLevel] || GeckoJS.Log.WARN;
 
             // addAppender for vivipos.log file.
-            GeckoJS.Log.addAppender('vivipos', new GeckoJS.Log.FileAppender(GeckoJS.Log.WARN, GeckoJS.Configure.read('CurProcD')+"/log/vivipos.log"));
+            GeckoJS.Log.addAppender('vivipos', new GeckoJS.Log.FileAppender(logLevelObj, GeckoJS.Configure.read('CurProcD')+"/log/vivipos.log"));
 
             GeckoJS.Log.defaultClassLevel = GeckoJS.Log[logLevel];
             GeckoJS.Log.getAppender('console').level = GeckoJS.Log[logLevel];
