98769fabe85d4b4c821be3c90ee9428be2243830
Irving Hsu <hsu.irving@gmail.com>
2010-12-07 01:43:52 +0800

    Saving work in progress on Black 1024x768 layout

--- chrome/content/layouts/black/black_prefs.js
+++ chrome/content/layouts/black/black_prefs.js
@@ -2,6 +2,8 @@
 function prefs_overlay_startup() {
     var prefwin = document.getElementById('prefwin');
     var displayPane = document.getElementById('displaySettingsPane');
+    var fixedFunctionPane = document.getElementById('fixedFunctionSettingsPane');
 
     if (displayPane) prefwin.addPane(displayPane);
+    if (fixedFunctionPane) prefwin.addPane(fixedFunctionPane);
 }
--- defaults/preferences/control_panel.js
+++ defaults/preferences/control_panel.js
@@ -3,3 +3,7 @@ pref("vivipos.fec.settings.controlpanels.system.registration.label", "chrome://r
 pref("vivipos.fec.settings.controlpanels.system.registration.path", "chrome://roundrock/content/registration/registration.xul");
 pref("vivipos.fec.settings.controlpanels.system.registration.roles", "acl_registration");
 
+pref("vivipos.fec.settings.controlpanels.config.fixedfunctionpanel.icon", "chrome://viviecr/skin/icons/icon_functionpnl.png");
+pref("vivipos.fec.settings.controlpanels.config.fixedfunctionpanel.label", "chrome://roundrock/locale/messages.properties");
+pref("vivipos.fec.settings.controlpanels.config.fixedfunctionpanel.path", "chrome://roundrock/content/layouts/black/fixedfuncpanelecrprefs.xul");
+pref("vivipos.fec.settings.controlpanels.config.fixedfunctionpanel.roles", "acl_manage_function_panel");
